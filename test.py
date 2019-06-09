bfc5ee6edd579eb2fedcb7ed646bf30e56c0d7a4
Oliver Bucher <level8broccoli@gmail.com>
2019-06-10 00:23:07 +0200

    forwarding and error messages

--- test.py
+++ test.py
@@ -1,27 +1,44 @@
-from flask import Flask
-from flask import request
+from flask import Flask, abort, request, redirect, url_for
+
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def hello():
-    return "<form action='/new/' method='POST'>" \
+    return "<form action='/' method='POST'>" \
         "  Input:<br>" \
         "  <input type='text' name='returnPOST' value='post-demo' />" \
         "  <br><br>" \
         "  <input type='submit' value='Submit' />" \
         "</form> "\
-        "<form action='/new/' method='GET'>" \
+        "<form action='/' method='GET'>" \
         "  Input:<br>" \
         "  <input type='text' name='returnGET' value='get-demo' />" \
         "  <br><br>" \
         "  <input type='submit' value='Submit' />" \
-        "</form> "
+        "</form> " +  \
+        print_out()
 
 
-@app.route("/new/", methods=['POST', 'GET'])
 def print_out():
-    if request.method == 'POST':
+    if request.method == 'GET' and request.args.get('returnGET') is not None:
+        return "Here is your String via GET method " + request.args.get('returnGET')
+    elif request.method == 'POST' and request.form['returnPOST'] is not None:
         return "Here is your String via POST method " + request.form['returnPOST']
     else:
-        return "Here is your String via GET method " + request.args.get('returnGET')
+        return ""
+
+
+@app.route('/also-forbidden')
+def also_forbidden():
+    return redirect(url_for('forbidden'))
+
+
+@app.route('/forbidden')
+def forbidden():
+    abort(404)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "huh" + str(error)
