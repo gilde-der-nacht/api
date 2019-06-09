95ec80bae55b322f4b3869015c6d2824b4d60430
Oliver Bucher <level8broccoli@gmail.com>
2019-06-10 00:04:51 +0200

    first tests for GET and POST methods

--- test.py
+++ test.py
@@ -1,7 +1,27 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "<form action='/new/' method='POST'>" \
+        "  Input:<br>" \
+        "  <input type='text' name='returnPOST' value='post-demo' />" \
+        "  <br><br>" \
+        "  <input type='submit' value='Submit' />" \
+        "</form> "\
+        "<form action='/new/' method='GET'>" \
+        "  Input:<br>" \
+        "  <input type='text' name='returnGET' value='get-demo' />" \
+        "  <br><br>" \
+        "  <input type='submit' value='Submit' />" \
+        "</form> "
+
+
+@app.route("/new/", methods=['POST', 'GET'])
+def print_out():
+    if request.method == 'POST':
+        return "Here is your String via POST method " + request.form['returnPOST']
+    else:
+        return "Here is your String via GET method " + request.args.get('returnGET')
