739ffb6f309da1fefc302058f0b4a4baa39089ba
Oliver Bucher <level8broccoli@gmail.com>
2019-06-10 15:07:19 +0200

    formatting

--- test.py
+++ test.py
@@ -4,21 +4,24 @@
 
 import json
 import urllib.request
-import unittest # TODO use unittest
+import unittest  # TODO use unittest
+
 
 def get(path):
-	req =  urllib.request.Request('http://127.0.0.1:5000{0}'.format(path))
-	result = urllib.request.urlopen(req).read()
-	print(result)
-	return result
+    req = urllib.request.Request('http://127.0.0.1:5000{0}'.format(path))
+    result = urllib.request.urlopen(req).read()
+    print(result)
+    return result
+
 
 def post(path, data):
-	req =  urllib.request.Request('http://127.0.0.1:5000{0}'.format(path), data=data, method='POST')
-	result = urllib.request.urlopen(req).read()
-	print(result)
-	return result
+    req = urllib.request.Request('http://127.0.0.1:5000{0}'.format(path), data=data, method='POST')
+    result = urllib.request.urlopen(req).read()
+    print(result)
+    return result
+
 
 assert get('/') == b'&#128154; Flask is running'
-assert get('/json-sender/1234') ==  b'{"uid": 5351, "public": "here are some public infos"}'
+assert get('/json-sender/1234') == b'{"uid": 5351, "public": "here are some public infos"}'
 assert post('/json-receiver/1234', b'{}') == b'1234'
 assert 'time' in json.loads(get('/status'))
