feba79f4eb68d2863037524718e665cc5e06e8c8
Oliver Bucher <level8broccoli@gmail.com>
2019-06-15 17:53:57 +0200

    added possible requests in

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # https://docs.python.org/3/library/urllib.request.html#module-urllib.request
+# requests module: https://2.python-requests.org/en/master/
 
 import requests
 from utility import status_codes
@@ -22,8 +23,8 @@ def put(path, data):
     return req.content, req.status_code
 
 
-def delete(path, data):
-    req = requests.delete('http://127.0.0.1:5000{0}'.format(path), data=data)
+def delete(path):
+    req = requests.delete('http://127.0.0.1:5000{0}'.format(path))
     return req.content, req.status_code
 
 
@@ -31,50 +32,109 @@ assert get('/') == (b'&#128154; Flask is running', status_codes.StatusCode.HTTP_
 
 # assert 'time' in json.loads(get('/status')) -> doesn't work for me
 
+new_resource_data = '''
+    {
+        "public_body": {"name": "Anmeldungen Rollenspieltage 2019"},
+        "private_body": {"email": "mail@abc.ch"}
+    }
+'''
+
+updated_resource_data = '''
+    {
+        "public_body": {"name": "Anmeldungen Rollenspieltage 2019 UPDATED"},
+        "private_body": {"email": "mail@xyz.ch"}
+    }
+'''
+
 # route '/resources'
 # GET
 assert get('/resources') == (b'check', status_codes.StatusCode.HTTP_200_OK)
 
 # POST
-data_01 = '''
-    {
-        "public_body": {"name": "Anmeldungen Rollenspieltage 2019"},
-        "private_body": {"email": "mail@xyz.ch"}
-    }
-'''
-new_resource = post('/resources', data_01)
-assert len(new_resource[0]) == 64
+new_resource = post('/resources', new_resource_data)
+created_resource_uid = new_resource[0]
+
+assert len(created_resource_uid) == 64
 assert new_resource[1] == status_codes.StatusCode.HTTP_201_CREATED
 
 # PUT
-assert put('/resources', data_01)[1] == status_codes.StatusCode.HTTP_405_METHOD_NOT_ALLOWED
+assert put('/resources', updated_resource_data)[1] == status_codes.StatusCode.HTTP_405_METHOD_NOT_ALLOWED
 
 # DELETE
-assert delete('/resources', data_01)[1] == status_codes.StatusCode.HTTP_405_METHOD_NOT_ALLOWED
-
+assert delete('/resources')[1] == status_codes.StatusCode.HTTP_405_METHOD_NOT_ALLOWED
 
 # route '/resources/<uid>'
 # GET
-get_existing_resources = get('/resources/{0}'.format(new_resource[0]))
+get_existing_resource = get('/resources/{0}'.format(created_resource_uid))
+assert get_existing_resource[0] == created_resource_uid
+assert get_existing_resource[1] == {"name": "Anmeldungen Rollenspieltage 2019"}
+assert get_existing_resource[2] == {"email": "mail@abc.ch"}
+assert get_existing_resource[3] == status_codes.StatusCode.HTTP_200_OK
 
-assert get_existing_resources[1] == status_codes.StatusCode.HTTP_200_OK
+get_non_existing_resource = get('/resources/{0}'.format('not_a_correct_uid'))
+assert get_non_existing_resource[2] == status_codes.StatusCode.HTTP_204_NO_CONTENT
 
+# POST
+assert post('/resources/{0}'.format(created_resource_uid), new_resource_data)[
+           1] == status_codes.StatusCode.HTTP_405_METHOD_NOT_ALLOWED
 
+# PUT
+assert put('/resources/{0}'.format(created_resource_uid), updated_resource_data)[
+           1] == status_codes.StatusCode.HTTP_201_CREATED
 
-# assert post('/post-json-to-container/uid/0123456789012345678901234567890101234567890123456789012345678901',
-#             b'{"public": [1, 2, 3], "private": [4, 5, 6]}') == b''
-#
-# get_json = json.loads(
-#     get('/get-json-from-container/uid/0123456789012345678901234567890101234567890123456789012345678901'))
-#
-# assert len(get_json) > 0
-#
-# for entry in get_json:
-#     assert 'timestamp' in entry
-#     assert 'public' in entry
-#     assert 'private' in entry
-#     assert 'entry_uid' in entry
-#     assert 'container_uid' in entry
+# DELETE
+assert delete('/resources/{0}'.format(created_resource_uid))[1] == status_codes.StatusCode.HTTP_201_CREATED
+
+new_entry_data = {
+    "public_body": {"events": ["Workshop: Unicorns in Gaming", "D&D: But with Lightsaber"]},
+    "private_body": {"name": "Hans Muster", "email": "mail@abc.ch"}
+}
+
+updated_entry_data = {
+    "public_body": {"events": ["Workshop: Unicorns in Gaming"]},
+    "private_body": {"name": "Hans Muster-Update", "email": "mail@xyz.ch"}
+}
+
+# route '/resources/<uid>/entries'
+# GET
+assert get('/resources/{0}/entries'.format(created_resource_uid)) == (b'check', status_codes.StatusCode.HTTP_200_OK)
+
+# POST
+new_entry = post('/resources/{0}/entries'.format(created_resource_uid), new_entry_data)
+created_entry_uid = new_entry[0]
+assert len(created_entry_uid) == 64
+assert new_entry[1] == status_codes.StatusCode.HTTP_201_CREATED
+
+# PUT
+assert put('/resources/{0}/entries'.format(created_resource_uid), updated_entry_data)[
+           1] == status_codes.StatusCode.HTTP_405_METHOD_NOT_ALLOWED
+
+# DELETE
+assert delete('/resources/{0}/entries'.format(created_resource_uid))[
+           1] == status_codes.StatusCode.HTTP_405_METHOD_NOT_ALLOWED
+
+# route '/resources/<uid>/entries/<uid>'
+# GET
+get_existing_entry = get('/resources/{0}/entries/{1}'.format(created_resource_uid, created_entry_uid))
+assert get_existing_entry[0] == created_entry_uid
+assert get_existing_entry[1] == {"events": ["Workshop: Unicorns in Gaming", "D&D: But with Lightsaber"]}
+assert get_existing_entry[2] == {"name": "Hans Muster", "email": "mail@abc.ch"}
+assert get_existing_entry[3] == status_codes.StatusCode.HTTP_200_OK
+
+get_non_existing_entry = get('/resources/{0}/entries/{1}'.format(created_resource_uid, 'not_a_correct_uid'))
+assert get_non_existing_entry[2] == status_codes.StatusCode.HTTP_204_NO_CONTENT
+
+# POST
+assert post('/resources/{0}/entries/{1}'.format(created_resource_uid, created_entry_uid), new_entry_data)[
+           1] == status_codes.StatusCode.HTTP_405_METHOD_NOT_ALLOWED
+
+# PUT
+assert put('/resources/{0}/entries/{1}'.format(created_resource_uid, created_entry_uid), updated_entry_data)[
+           1] == status_codes.StatusCode.HTTP_201_CREATED
+
+# DELETE
+assert delete('/resources/{0}/entries/{1}'.format(created_resource_uid, created_entry_uid))[
+           1] == status_codes.StatusCode.HTTP_201_CREATED
 
 # TODO test invalid id
 # TODO add api version to status, use semantic versioning
