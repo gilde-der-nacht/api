3d287564597a3100619b1459b4dfaf8a556f3b78
Oliver Bucher <level8broccoli@gmail.com>
2019-06-22 18:41:49 +0200

    deleting old code

--- flask-app.py
+++ flask-app.py
@@ -51,8 +51,7 @@ def resources():
         abort(requests.codes.METHOD_NOT_ALLOWED)
 
     if request.method == 'GET':
-        return 'check'
-        # all_resources = storage.read_all('resources')
+        return storage.read_test_resource('095da522f49aebbd35443fd2349d578a1aaf4a9ea05ae7d59383a5f416d4fd3b')
 
     # TODO use an elif, if it was a GET it is impossible to be a POST
     if request.method == 'POST':
@@ -86,83 +85,6 @@ def entries(resource_uid):
         entry_uid = entry.get('uid')
 
         return entry_uid, requests.codes.CREATED
-
-
-# @app.route('/post-json-to-container/uid/<uid>', methods=['POST'])
-# def post_json_to_container(uid):
-#     # naming, we do not return a container we actually return the entries, /container/.../<uid> may return some statistics about the container or nothing at all
-#
-#     if request.method != 'POST':
-#         abort(status_codes.StatusCode.HTTP_405_METHOD_NOT_ALLOWED)
-#
-#     try:
-#         # you used a 'form' here, but this measn we have to generate a form with JS and inside the test, maybe this is the better idea, but client/server need to be both updated then, we can avoid the dumps so it may be worth to investigate
-#         received_data = request.get_data()
-#         received_json = json.loads(received_data)
-#
-#         public_body = json.dumps(received_json['public'])
-#         private_body = json.dumps(received_json['private'])
-#
-#         storage.write(uid, public_body, private_body)
-#
-#     except ValueError:
-#         abort(status_codes.StatusCode.HTTP_400_BAD_REQUEST)
-#
-#     return '{}'
-
-
-# @app.route('/get-json-from-container/uid/<uid>')
-# def get_json_from_container(uid):
-#     # TODO exception handling?
-#     # TODO use ''.format or f''
-#
-#     try:
-#         container = storage.read(uid)
-#     except ValueError:
-#         abort(status_codes.StatusCode.HTTP_400_BAD_REQUEST)
-#
-#     container_list = []
-#
-#     if False:
-#         for row in container:
-#             # I removed the logic to merge container, ... what if we later add another method to return all entries? maybe we like to use the same code to display then, but in this case we have to differentiate between two possible results
-#             container_list.append(
-#                 '''
-#                     {
-#                         'container_uid': '%s',
-#                         'entry_uid': '%s',
-#                         'public': '%s',
-#                         'private': '%s',
-#                         'timestamp': '%s'
-#                     }
-#                 ''' % (row[0], row[1], row[2], row[3], row[4])
-#
-#             )
-#             print(row)
-#
-#         json_output = '[' + ', '.join(container_list) + ']';
-#
-#     # simpler?
-#
-#     if False:
-#         def convert(entry):
-#             return '{{'container_uid': '{0}', 'entry_uid': '{1}', 'public': '{2}', 'private': '{3}', 'timestamp': '{4}'}}'.format(
-#                 *entry)
-#
-#         container_list = map(convert, container)
-#         json_output = '[' + ', '.join(container_list) + ']';
-#
-#     # better? we do not assemble json ourself ...
-#
-#     if True:
-#         def convert(entry):
-#             return {'container_uid': entry[0], 'entry_uid': entry[1], 'public': json.loads(entry[2]),
-#                     'private': json.loads(entry[3]), 'timestamp': entry[4]}
-#
-#         container_list = map(convert, container)
-#         json_output = json.dumps(list(container_list))
-#
-#     return json_output
 
 
 # TODO authentication
