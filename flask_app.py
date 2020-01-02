6de67f9273825ccb74d8b136643fba56fae29332
Oliver Bucher <level8broccoli@gmail.com>
2020-01-02 17:11:43 +0100

    debbuging

--- flask_app.py
+++ flask_app.py
@@ -155,7 +155,7 @@ def get_recipients(resource_uid):
     try:
         resource = storage.resources_list_single(resource_uid)
         recipients = [resource[3]['email']]
-        return resource
+        recipients = resource
     except:
         recipients = None
     return recipients
